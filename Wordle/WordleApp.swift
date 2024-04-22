aa4511b0b9d450f8d1c91a95acf4e0e98219563c
Jada Crockett <jada.s.crockett@gmail.com>
2024-04-22 17:43:49 -0400

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Jada Crockett on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
